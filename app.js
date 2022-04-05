67e21bf5198b0e1ba81d4e95de972768fa4f8fd1
Mohammed Al-Rasheed <mohammed.satar.alrasheed@gmail.com>
2022-04-05 04:34:06 -0400

    allow cors

--- app.js
+++ app.js
@@ -30,12 +30,25 @@ con.on("open", () => {
 app.use(express.json());
 // add helmet to protect the app from some common vulnerabilities
 app.use(helmet());
+// CORS error	
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
+  next();
+});
 
 app.use("/courses", coursesRouter);
 app.use("/notes", notesRouter);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-  console.log("listening on port 9000");
+  console.log("listening on port 8080");
 });
